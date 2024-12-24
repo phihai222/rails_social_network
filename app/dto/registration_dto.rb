c307025aefb542841923ab62fa41ce1471ed7d92
phihai91 <phihai91@outlook.com>
2024-12-24 10:48:41 +0700

    #auth #registration validate email and password regex

--- app/dto/registration_dto.rb
+++ app/dto/registration_dto.rb
@@ -2,6 +2,18 @@ class RegistrationDto
   include ActiveModel::Model
   attr_accessor :email, :password
 
-  validates :email, presence: true, length: { maximum: 100 } #TODO validate email
-  validates :password, presence: true, length: { minimum: 6 } # TODO make a regex here
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  STRONG_PASSWORD_REGEX = /\A
+    (?=.*[A-Z])       # At lease one letter uppercase
+    (?=.*[a-z])       # At lease one letter lowercase
+    (?=.*\d)          # At lease one number
+    (?=.*[@#$%^&*])   # At lease one special character
+    .{8,}             # Minimum length 8
+  \z/x
+
+  validates :email, presence: true, length: { maximum: 100 },
+            format: { with: EMAIL_REGEX, message: "is not a valid email address" }
+  validates :password, presence: true, length: { minimum: 6 },
+            format: { with: STRONG_PASSWORD_REGEX, message: "password must be strong" }
 end
